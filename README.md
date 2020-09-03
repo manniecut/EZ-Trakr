1bc0751689abcce3f66b801d5bf0e1b9e859ae9a
Mannie Cutean <68138888+manniecut@users.noreply.github.com>
2020-09-03 17:29:58 -0400

    added save and load functionality

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 API project
 
 
-This will be an app that keeps track of all of your packages that are in transit.
+This will is an app that keeps track of all of your packages that are in transit.
+
+You can enter your tracking number, select the carrier, and add a nickname to keep tabs on your package.
+
+This is currently in the minimally viable product phase, and has some test items in there for the grader to utilize.
+
+In the next phase I would also like to implement the ability to store the list on local storage, but I t
--- index.js
+++ index.js
@@ -1,49 +1,67 @@
-/*
- TO DO
-    -add store cookie functionality
-    -show/hide description and map
-*/
-
-
-
 'use strict';
 
 const apiKeyTracking = "c5128df5b0msh18188fd61436bc7p13b97fjsn8f4c632a3599";
-const apiKeyMaps = "AIzaSyC5q4AzjnRDhf5XXXXXXXXX3nceGVJBJPRbBVZyDS5U"
+const apiKeyMaps = "AIzaSyC5q4AzjnRDhf53nceGVJBJPRbBVZyDS5U"
 
 const searchUrl = "https://order-tracking.p.rapidapi.com/trackings/realtime";
 
+
 const STORE = [
     {
-        nickName: "Drone Parts from GetFPV",
-        trackingNum: "9405511108296806521993",
-        status: "DELIVERED",
-        date: "2020-08-28 16:21",
-        description: "Your fake package is in or at your doorstep",
-        location: "ROMULUS, MI - USA",
-        carrier: "usps"
+        nickName: "null",
+        trackingNum: "null",
+        status: "null",
+        date: "null",
+        description: "null",
+        location: "null",
+        carrier: "null"
     },
-    {
-        nickName: "Books from eBay",
-        trackingNum: "8342704440",
-        status: "TRANSIT",
-        date: "2020-09-01 04:11:00",
-        description: "Arrived at Sort Facility HONG KONG - HONG KONG SAR, CHINA",
-        location: "HONG KONG - HONG KONG SAR, CHINA",
-        carrier: "dhl"
-    }
 ];
+
+
+
+/*********************** SAVE/LOAD FUNCTIONS ****************************************************/
+
+function loadLocalStorage() {
+    console.log('function loadLocalStorage()')
+    let currentLength = STORE.length;
+    if (currentLength > 0) {
+        console.log('parsing local storage')
+        let loadedStore = JSON.parse(localStorage.getItem("storeString")); //create a variable that contains the parsed stored item
+        console.log('the loaded storage is:');
+        console.log(loadedStore);
+        STORE.splice(0, currentLength);
+        console.log('store spliced');
+        for (let i = 0; i < loadedStore.length; i++) {
+            STORE.push(loadedStore[i]);
+            console.log('loading stored object into STORE')
+        }
+        console.log('loading complete')
+    } else {
+        console.log('nothing saved');
+    };
+};
+
+function saveToLocalStorage() {
+    console.log('function saveToLocalStorage()');
+    let storeString = JSON.stringify(STORE);
+    console.log('storeString =');
+    console.log(storeString);
+    localStorage.setItem("storeString", storeString);
+    console.log('localStorage =')
+    console.log(localStorage);
+}
 
 
 
 /*********************** LIST FUNCTIONS ****************************************************/
 
 function generateItemElement(item) {              //formats the STORED items for use in the list
-    console.log('generateItemElement');
+    console.log(`function generateItemElement() for ${item.nickName}`);
     let mapDisplay = getMap(item.location)
     return `
     <li data-item-id="${item.trackingNum}">
-      <span class="package-item js-package-item">${item.trackingNum}</span>
+      <span class="package-item js-package-item">${item.trackingNum}/${item.carrier}</span>
             <h3>${item.nickName}</h3>
             <p>${item.status} - ${item.date}</p>
             <p>${item.description}</p>
@@ -58,19 +76,20 @@ function generateItemElement(item) {              //formats the STORED items for
 };
 
 function generatePackageItemsString(trackingStore) { // combines html elements generated from STORE items and returns them ready to be displayed
-    console.log('generatePackageItemsString');
+    console.log('function generatePackageItemsString()');
     const items = trackingStore.map((item) => generateItemElement(item));
     return items.join("");
 };
 
 function renderPackageList() { // displays result of generatePackageItemsString in html area
-    console.log('renderPackageList');
+    saveToLocalStorage(STORE);
+    console.log('function renderPackageList()');
     const packageListItemsString = generatePackageItemsString(STORE);
     $('.js-package-list').html(packageListItemsString);     // this is the combined formatted HTML strings from STORE
 };
 
 function getItemIdFromElement(item) {
-    console.log('getItemIdFromElement');
+    console.log('function getItemIdFromElement()');
     return $(item)
         .closest('li')
         .data('item-id');
@@ -81,50 +100,41 @@ function getItemIdFromElement(item) {
 /********* LIST ITEM BUTTONS *********/
 
 function deleteListItem(itemId) {
-    console.log(`deleteListItem`)
+    console.log(`**DELETE**`)
     const itemIndex = STORE.findIndex(item => item.trackingNum === itemId);
     STORE.splice(itemIndex, 1);
 }
 
 function handleDeleteItem() {
-    console.log('handleDeleteItem');
     $('.js-package-list').on('click', '.js-item-delete', event => {
-        console.log('clicked remove')
         const itemId = getItemIdFromElement(event.currentTarget);
         deleteListItem(itemId);
         renderPackageList();
-
     });
 }
 
-
 function refreshList() {
+    console.log('**REFRESH**');
     let previousData = STORE;
     for (let i = 0; i < previousData.length; i++) {
-        STORE.splice(i, 1);
-        getPackageInfo(previousData[i].trackingNum, previousData[i].carrier, previousData[i].nickName)
-    }
-    //loop through store items
-    //for each item, run getpackageinfo with properinputs
-    //clear list, but when? maybe once per item during the loop
-    
-
-}
+        getPackageInfo(previousData[i].trackingNum, previousData[i].carrier, previousData[i].nickName);
+    };
+    STORE.splice(0, previousData.length);
+};
 
 function handleRefreshButton() {
     $('#packages').on('click', '.js-list-refresh', event => {
-        console.log('clicked refresh')
         refreshList();
-
     });
 
 }
+
 
 
 /********************* ADDING RESPONSE INFORMATION TO THE LIST *****************************/
 
 function addNumToStore(responseJson, packageNickName) {     //adds information from responseJson to the STORE
-    console.log(`Adding "${packageNickName}" to package list`);
+    console.log(`function addNumToStore() for "${packageNickName}"`);
     STORE.push({
         nickName: packageNickName,
         trackingNum: responseJson.data.items[0].tracking_number,
@@ -137,7 +147,7 @@ function addNumToStore(responseJson, packageNickName) {     //adds information f
 }
 
 function displayResults(responseJson, packageNickName) {   //stores response data and refreshes the displayed list
-    console.log('displayResults')
+    console.log(`function displayResults() for "${packageNickName}`);
     console.log(responseJson)
     addNumToStore(responseJson, packageNickName);
     renderPackageList();
@@ -147,10 +157,8 @@ function displayResults(responseJson, packageNickName) {   //stores response dat
 
 /************************ REQUEST FUNCTIONS *********************************************/
 
-
 function getPackageInfo(newTrackingNum, carrier, packageNickName) { //gets the information then runs JSON through displayReults
-    console.log('running getPackageInfo');
-    console.log(carrier);
+    console.log(`function getPackageInfo() for ${packageNickName}`);
     var myHeaders = new Headers();
     myHeaders.append("x-rapidapi-host", " order-tracking.p.rapidapi.com");
     myHeaders.append("x-rapidapi-key", ` ${apiKeyTracking}`);
@@ -170,27 +178,26 @@ function getPackageInfo(newTrackingNum, carrier, packageNickName) { //gets the i
         .then(response => response.json())
         .then(responseJson => displayResults(responseJson, packageNickName))
         .catch(error => alert('Please check that your tracking number and carrier match and try again.'));
-    console.log('request sent');
+    console.log('FETCH request sent');
 };
 
 function getMap(location) {
-    console.log('getting map');
+    console.log('function getMap()');
     let locationEncoded = encodeURI(location)
     console.log(locationEncoded);
     let mapsURL = `https://www.google.com/maps/embed/v1/search?key=${apiKeyMaps}&q=${locationEncoded}`
-    console.log(mapsURL);
+    console.log('retrieving Map from ' + mapsURL);
     return mapsURL;
 
 }
 
 
 
-
-
-
 /************************* EVENT LISTENERS **********************************************/
 
 function handlePackageList() {
+    console.log('SCRIPT INITIATING')
+    loadLocalStorage();
     renderPackageList();
     handleDeleteItem();
     handleRefreshButton();
@@ -203,7 +210,7 @@ function watchForm() {
         const newTrackingNum = $('#js-number-input').val();
         const carrier = $('#js-carrier-input').val();
         const packageNickName = $('#js-nickname-input').val();
-        console.log('input received')
+        console.log('**NEW SUBMISSION**')
         getPackageInfo(newTrackingNum, carrier, packageNickName);
     })
 };
